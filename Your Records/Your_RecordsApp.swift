c7e8475efc8f4a7f50780eec31183c7f7fc52aab
LuckDuracell <lukedru@yahoo.com>
2021-08-23 17:14:20 -0700

    Initial Commit

---
+++ Your Records/Your_RecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_RecordsApp.swift
+//  Your Records
+//
+//  Created by Luke Drushell on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Your_RecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
