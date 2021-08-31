972f26d797d87ae316a0390d0829cf770d1ba2dd
LuckDuracell <lukedru@yahoo.com>
2021-08-30 23:20:54 -0700

    trying to pull workout data, not working atm

--- Your Records/ContentView.swift
+++ Your Records/ContentView.swift
@@ -9,85 +9,71 @@ import SwiftUI
 import HealthKit
 import HealthKitUI
 
-
-
-func fetchHealthData() -> Void {
+func authorize() {
     let healthStore = HKHealthStore()
-    if HKHealthStore.isHealthDataAvailable() {
-        let readData = Set([
-            HKObjectType.quantityType(forIdentifier: .heartRate)!
-        ])
-        
-        healthStore.requestAuthorization(toShare: [], read: readData) { (success, error) in
-            if success {
-                let calendar = NSCalendar.current
-                
-                var anchorComponents = calendar.dateComponents([.day, .month, .year, .weekday], from: NSDate() as Date)
-                
-                let offset = (7 + anchorComponents.weekday! - 2) % 7
-                
-                anchorComponents.day! -= offset
-                anchorComponents.hour = 2
-                
-                guard let anchorDate = Calendar.current.date(from: anchorComponents) else {
-                    fatalError("*** unable to create a valid date from the given components ***")
-                }
-                
-                let interval = NSDateComponents()
-                interval.minute = 30
-                                    
-                let endDate = Date()
-                                            
-                guard let startDate = calendar.date(byAdding: .month, value: -1, to: endDate) else {
-                    fatalError("*** Unable to calculate the start date ***")
-                }
-                                    
-                guard let quantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
-                    fatalError("*** Unable to create a step count type ***")
-                }
- 
-                let query = HKStatisticsCollectionQuery(quantityType: quantityType,
-                                                        quantitySamplePredicate: nil,
-                                                            options: .discreteAverage,
-                                                            anchorDate: anchorDate,
-                                                            intervalComponents: interval as DateComponents)
-                
-                query.initialResultsHandler = {
-                    query, results, error in
-                    
-                    guard let statsCollection = results else {
-                        fatalError("*** An error occurred while calculating the statistics: \(String(describing: error?.localizedDescription)) ***")
-                        
-                    }
-                                        
-                    statsCollection.enumerateStatistics(from: startDate, to: endDate) { statistics, stop in
-                        if let quantity = statistics.averageQuantity() {
-                            let date = statistics.startDate
-                            //for: E.g. for steps it's HKUnit.count()
-                            let value = quantity.doubleValue(for: HKUnit(from: "count/min"))
-                            print("done")
-                            print(value)
-                            print(date)
-                                                        
-                        }
-                    }
-                    
-                }
-                
-                healthStore.execute(query)
-                
-            } else {
-                print("Authorization failed")
- 
-            }
+    let allTypes = Set([HKObjectType.workoutType(),
+                        HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                        HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
+                        HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                        HKObjectType.quantityType(forIdentifier: .heartRate)!])
+    healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+        if !success {
+            // Handle the error here.
+            print("authorizationError")
         }
     }
 }
 
+
+func loadDistanceWorkouts(completion:  @escaping ([HKWorkout]?, Error?) -> Void) {
+    guard let distanceType =
+        HKObjectType.quantityType(forIdentifier:
+                                    HKQuantityTypeIdentifier.heartRate) else {
+                fatalError("*** Unable to create a Heart rate type ***")
+    }
+        // Get all workouts that only came from this app.
+    let workoutPredicate = HKQuery.predicateForObjects(from: .default())
+
+    let startDateSort = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)
+
+    let query = HKSampleQuery(sampleType: distanceType,
+                              predicate: workoutPredicate,
+                              limit: 0,
+                              sortDescriptors: [startDateSort]) { (sampleQuery, results, error) -> Void in
+                                guard let distanceSamples = results as? [HKQuantitySample] else {
+                                    // Perform proper error handling here.
+                                    return
+                                }
+                                // Use the workout's distance samples here.
+                                print("\(distanceSamples)")
+    }
+
+    HKHealthStore().execute(query)
+    
+    }
+
 struct ContentView: View {
+    
+    @State var distanceOutput = ""
+    
+    
     var body: some View {
-        Button(action: fetchHealthData) {
-                        Text("Fetch data")
+        if distanceOutput != "" {
+            Text(distanceOutput)
+        }
+        Button {
+            authorize()
+            let healthStore = HKHealthStore()
+            if healthStore.authorizationStatus(for: HKObjectType.workoutType()) == HKAuthorizationStatus.sharingAuthorized {
+                print("success BABY")
+                loadDistanceWorkouts(completion: { (success, error) in
+                    if (success == nil) {
+                        print("completionError")
+                    }
+                })
+            }
+        } label: {
+            Text("Fetch Data")
         }
     }
 }
